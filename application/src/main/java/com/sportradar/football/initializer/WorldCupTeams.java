37dd7244df11adbdfeb7c8b8d65a35df7c1662b5
ketled <devanshu.ketle@posten.no>
2022-05-18 01:00:15 +0200

    Initialize the list of teams

---
+++ application/src/main/java/com/sportradar/football/initializer/WorldCupTeams.java
@@ -0,0 +1,22 @@
+package com.sportradar.football.initializer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorldCupTeams {
+
+    public static List<String> listOfTeams() {
+        List<String> listOfTeams = new ArrayList<>();
+        listOfTeams.add("Mexico");
+        listOfTeams.add("Canada");
+        listOfTeams.add("Spain");
+        listOfTeams.add("Brazil");
+        listOfTeams.add("Germany");
+        listOfTeams.add("France");
+        listOfTeams.add("Uruguay");
+        listOfTeams.add("Italy");
+        listOfTeams.add("Argentina");
+        listOfTeams.add("Australia");
+        return listOfTeams;
+    }
+}
